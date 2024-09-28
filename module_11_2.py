14e2e0ae158a8ab67321a5b36a3afe9efa9ce01e
SirEvgen <sandrofa6666666@gmail.com>
2024-09-29 02:43:54 +0300

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -5,7 +5,7 @@ from pprint import pprint
 def introspection_info(obj):
     result = {}
     result['type'] = type(obj)
-    result['attributes'] = dir(obj)
+    result['attributes'] = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
     result['methods'] = [method for method in dir(type(obj)) if callable(getattr(obj, method))]
     result['module'] = inspect.getmodule(introspection_info)
     return result
